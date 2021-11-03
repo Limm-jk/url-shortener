a1f2dcb7295425b172d38e1c2032f7751bb6755b
limm-jk <junkyu.lim.dev@gmail.com>
2021-11-03 15:00:06 +0900

    Feat : URL encode / decode 함수 추가

---
+++ src/main/java/com/limm/urlshortener/util/UrlUtils.java
@@ -0,0 +1,25 @@
+package com.limm.urlshortener.util;
+
+public class UrlUtils {
+    final int RADIX = 62;
+    final String CODEC = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    public String encoding(long param) {
+        StringBuilder sb = new StringBuilder();
+        while(param > 0) {
+            sb.append(CODEC.charAt((int) (param % RADIX)));
+            param /= RADIX;
+        }
+        return sb.toString();
+    }
+
+    public long decoding(String param) {
+        long sum = 0;
+        long power = 1;
+        for (int i = 0; i < param.length(); i++) {
+            sum += CODEC.indexOf(param.charAt(i)) * power;
+            power *= RADIX;
+        }
+        return sum;
+    }
+}
