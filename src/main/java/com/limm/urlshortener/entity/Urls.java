3f74417d769d50a824de8666744755055ee5cd08
limm-jk <junkyu.lim.dev@gmail.com>
2021-11-05 02:19:44 +0900

    Feat : Urls 엔티티 추가

---
+++ src/main/java/com/limm/urlshortener/entity/Urls.java
@@ -0,0 +1,30 @@
+package com.limm.urlshortener.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Urls {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String url;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    public Urls(String url) {
+        this.url = url;
+    }
+}
