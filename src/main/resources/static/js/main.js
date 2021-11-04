55ac50f316e5fad909bac868a02af626942b099d
limm-jk <junkyu.lim.dev@gmail.com>
2021-11-05 04:44:38 +0900

    Chore : short url 생성 시 alert 추가

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -11,6 +11,7 @@ function dataSend() {
         data: messageDTO,
         type:"POST",
     }).done(function (fragment) {
+        top.alert("Short Url이 성공적으로 생성되었습니다.");
         $("#resultDiv").replaceWith(fragment);
     });
 }
